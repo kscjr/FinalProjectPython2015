1c946c699a352633a6dfc4c4b78b543f57bb5872
kscjr <kclements15@archmereacademy.com>
2015-01-08 13:46:53 -0500

    Update platformer.py
    Changed the color of the rectangle dude to purple, changed the background image, added some moving platforms, added a wall at the end to give a point to the platforming, added a third level. The platforms get thinner in each level. Couldn't figure out how to print "you win" text.

--- Desktop/Archmere/Game Design/Programs/pyGame/platformer/platformer.py
+++ Desktop/Archmere/Game Design/Programs/pyGame/platformer/platformer.py
@@ -7,9 +7,11 @@ WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
+PURPLE = (255, 0, 255)
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+
 
 class Player(pygame.sprite.Sprite):
     change_x = 0
@@ -23,7 +25,7 @@ class Player(pygame.sprite.Sprite):
         width = 40
         height = 40
         self.image = pygame.Surface([width, height])
-        self.image.fill(RED)
+        self.image.fill(PURPLE)
 
         self.rect = self.image.get_rect()
 
@@ -49,7 +51,7 @@ class Player(pygame.sprite.Sprite):
                 self.rect.top = block.rect.bottom
 
             self.change_y = 0
-            
+        
 
     def calc_grav(self):
         if self.change_y == 0:
@@ -78,6 +80,8 @@ class Player(pygame.sprite.Sprite):
     def stop(self):
         self.change_x = 0
 
+
+
 class Platform(pygame.sprite.Sprite):
     def __init__(self, width, height):
         pygame.sprite.Sprite.__init__(self)
@@ -86,6 +90,45 @@ class Platform(pygame.sprite.Sprite):
         self.image.fill(GREEN)
 
         self.rect = self.image.get_rect()
+
+class platform2(Platform):
+
+    change_x = 0
+    change_y = 0
+
+    boundary_top = 0
+    boundary_bottom = 0
+    boundary_left = 0
+    boundary_right = 0
+
+    player = None
+    level = None
+
+    def update(self):
+        self.rect.x += self.change_x
+
+        hit = pygame.sprite.collide_rect(self, self.player)
+        if hit:
+            if self.change_x < 0:
+                self.player.rect.right = self.rect.left
+            else:
+                self.player.rect.left = self.rect.right
+        self.rect.y += self.change_y
+
+        hit = pygame.sprite.collide_rect(self, self.player)
+        if hit:
+            if self.change_y< 0:
+                self.player.rect.bottom = self.rect.top
+            else:
+                self.rect.top = self.rect.bottom
+
+        if self.rect.bottom > self.boundary_bottom or self.rect.top < self.boundary_top:
+                self.change_y *= -1
+        cur_pos = self.rect.x - self.level.world_shift
+        if cur_pos < self.boundary_left or cur_pos > self.boundary_right:
+                self.change_x *=-1 
+        
+    
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, width, height):
@@ -97,6 +140,7 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
 class Level():
+    level = "0"
     platform_list = None
     enemy_list = None
 
@@ -112,11 +156,13 @@ class Level():
         self.enemy_list.update()
 
     def draw(self, screen):
+        
+
         background_image = pygame.image.load("marioBackground.jpg")
         background_fit = pygame.transform.scale(background_image, (800, 600))       
 
         screen.blit(background_fit, [0, 0])
-
+    
         self.platform_list.draw(screen)
         self.enemy_list.draw(screen)
 
@@ -138,7 +184,8 @@ class Level_01(Level):
         level = [[210, 70, 500, 500],
                  [210, 70, 800, 400],
                  [210, 70, 1000, 500],
-                 [210, 70, 1120, 280]]
+                 [210, 70, 1120, 280],
+                 [120, 500, 1700, 200]]
 
         enemylist = [[105, 35, 100, 100],
                      [105, 35, 200, 200]]
@@ -149,6 +196,16 @@ class Level_01(Level):
             block.rect.y = platform[3]
             block.player = self.player
             self.platform_list.add(block)
+
+        block = platform2(70, 40)
+        block.rect.x = 1350
+        block.rect.y = 280
+        block.boundary_left = 1350
+        block.boundary_right = 1600
+        block.change_x = 1
+        block.player = self.player
+        block.level = self
+        self.platform_list.add(block)
 
         for enemy in enemylist:
             block = Enemy(enemy[0], enemy[1])
@@ -166,7 +223,8 @@ class Level_02(Level):
         level = [[210, 30, 450, 570],
                  [210, 30, 850, 420],
                  [210, 30, 1000, 520],
-                 [210, 30, 1120, 280]]
+                 [210, 30, 1120, 280],
+                 [120, 500, 1700, 200]]
 
         for platform in level:
             block = Platform(platform[0], platform[1])
@@ -174,6 +232,58 @@ class Level_02(Level):
             block.rect.y = platform[3]
             block.player = self.player
             self.platform_list.add(block)
+
+        block = platform2(70, 70)
+        block.rect.x = 1500
+        block.rect.y = 300
+        block.boundary_top = 100
+        block.boundary_bottom = 550
+        block.change_y = -1
+        block.player = self.player
+        block.level = self
+        self.platform_list.add(block)
+
+class Level_03(Level):
+    def __init__(self, player):
+        Level.__init__(self, player)
+
+        self.level_limit = -1000
+
+        level = [[210, 10, 475, 470],
+                 [210, 10, 800, 320],
+                 [210, 10, 900, 420],
+                 [210, 10, 1020, 180],
+                 [120, 500, 1700, 200]]
+
+        for platform in level:
+            block = Platform(platform[0], platform[1])
+            block.rect.x = platform[2]
+            block.rect.y = platform[3]
+            block.player = self.player
+            self.platform_list.add(block)
+
+        block = platform2(70, 70)
+        block.rect.x = 1500
+        block.rect.y = 300
+        block.boundary_top = 100
+        block.boundary_bottom = 550
+        block.change_y = -1
+        block.player = self.player
+        block.level = self
+        self.platform_list.add(block)
+
+
+class Level_04(Level):
+    def __init__(self, player):
+
+        
+        
+        Level.__init__(self, player)
+
+        self.level_limit = -1000
+
+
+
 
 def main():
     pygame.init()
@@ -188,6 +298,8 @@ def main():
     level_list = []
     level_list.append(Level_01(player))
     level_list.append(Level_02(player))
+    level_list.append(Level_03(player))
+    level_list.append(Level_04(player))
 
     current_level_no = 0
     current_level = level_list[current_level_no]
@@ -226,6 +338,8 @@ def main():
         active_sprite_list.update()
 
         current_level.update()
+
+
 
         if player.rect.right >= 500:
             diff = player.rect.right - 500
